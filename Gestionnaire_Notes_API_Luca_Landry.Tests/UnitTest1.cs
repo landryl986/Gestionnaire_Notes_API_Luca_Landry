fa09daaf612ea829fb08b5e42d2c89aa29c68902
Luca Landry <lucalandry@icloud.com>
2021-01-12 11:14:01 +0100

    test directory created

---
+++ Gestionnaire_Notes_API_Luca_Landry.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gestionnaire_Notes_API_Luca_Landry.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
