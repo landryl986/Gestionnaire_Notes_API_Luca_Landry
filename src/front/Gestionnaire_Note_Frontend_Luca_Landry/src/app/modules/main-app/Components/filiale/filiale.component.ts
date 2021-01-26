8b8965ddf2d0ef371093dece50e20cdcac2c171f
Luca Landry <lucalandry@icloud.com>
2021-01-26 15:51:27 +0100

    Il faut géré le tableau filiale

--- src/front/Gestionnaire_Note_Frontend_Luca_Landry/src/app/modules/main-app/Components/filiale/filiale.component.ts
+++ src/front/Gestionnaire_Note_Frontend_Luca_Landry/src/app/modules/main-app/Components/filiale/filiale.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PhilialService} from '../../../../../Services/philial.service';
+import {IPhilial} from "../../../../../Interfaces/IPhilial";
+import {ICreateUserDto} from "../../../../../Interfaces/ICreateUserDto";
 
 @Component({
   selector: 'app-filiale',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filiale.component.scss']
 })
 export class FilialeComponent implements OnInit {
+  filiale: IPhilial;
+  filiales: Array<IPhilial>;
+  id: number;
 
-  constructor() { }
+  constructor(private service: PhilialService) {
+    this.filiale = {} as IPhilial;
+  }
 
   ngOnInit(): void {
+    this.GetAll();
+  }
+
+  addFilial(): void {
+    this.filiale.userId = Number(localStorage.getItem('Id'));
+
+    this.service.Add(this.filiale).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
+    this.filiale = {} as IPhilial;
+  }
+
+  GetAll(): void
+  {
+    this.id = Number(localStorage.getItem('Id'));
+    this.service.GetAllByUser(this.id).subscribe
+    (
+      data =>
+      {
+        if (data)
+        {
+          this.filiales = data['result'];
+        }
+      },
+      error => {}
+    );
   }
 
 }
